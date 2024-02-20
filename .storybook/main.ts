ccc5f5ff745755ef829c9c53da114bc0efb23cbc
FahadAminShovon <29832989+FahadAminShovon@users.noreply.github.com>
2024-02-20 19:06:51 +0600

    :wrench: setup mui with storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,49 @@
-import type { StorybookConfig } from "@storybook/react-vite";
+import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  // stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: [
+    '../src/**/*.mdx',
+    {
+      directory: '../src/@ui/components/',
+      files: '**/*.stories.@(ts|tsx)',
+      titlePrefix: 'Components',
+    },
+    {
+      directory: '../src/@ui/modules/',
+      files: '**/*.stories.@(ts|tsx)',
+      titlePrefix: 'Modules',
+    },
+  ],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-interactions',
+    '@storybook/addon-themes',
+    '@storybook/themes',
   ],
   framework: {
-    name: "@storybook/react-vite",
+    name: '@storybook/react-vite',
     options: {},
   },
   docs: {
-    autodocs: "tag",
+    // autodocs: 'tag',
+    autodocs: true,
+  },
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      // Makes union prop types like variant and size appear as select controls
+      shouldExtractLiteralValuesFromEnum: true,
+      // Makes string and boolean types that can be undefined appear as inputs and switches
+      shouldRemoveUndefinedFromOptional: true,
+      // Filter out third-party props from node_modules except @mui packages
+      propFilter: (prop) =>
+        prop.parent
+          ? !/node_modules\/(?!@mui)/.test(prop.parent.fileName)
+          : true,
+    },
   },
 };
 export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,15 +1,43 @@
-import type { Preview } from "@storybook/react";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/material-icons';
+
+import type { Preview } from '@storybook/react';
+
+import { ThemeProvider, CssBaseline, createTheme } from '@mui/material';
+import { withThemeFromJSXProvider } from '@storybook/addon-themes';
+
+/* TODO: update import for your custom Material UI themes */
+// import { lightTheme, darkTheme } from '../path/to/themes';
+import { theme } from '../src/theme';
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
+      expanded: true,
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
     },
   },
+
+  decorators: [
+    withThemeFromJSXProvider({
+      GlobalStyles: CssBaseline,
+      Provider: ThemeProvider,
+      themes: {
+        // Provide your custom themes here
+        // light: theme,
+        // dark: theme,
+        theme,
+      },
+      defaultTheme: 'light',
+    }),
+  ],
 };
 
 export default preview;
